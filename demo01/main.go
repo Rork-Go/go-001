8fcac800de9d11684f281c7f4b0cf1060892c9f7
Rocky <614214698@qq.com>
2025-10-16 18:30:20 +0800

    222

--- demo01/main.go
+++ demo01/main.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main() {
-  var a int = 10
-  var b int = 20
-  var c int = a + b
-  fmt.Println(c)
+	var a int = 10
+	var b int = 20
+	var c int = a + b
+	fmt.Println(c)
+
+	var d int = 10
+	var e int = 20
+	var f int = d * e
+	fmt.Println(f)
 }
